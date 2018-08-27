26a981808e186aa07c2e01fa3806424e23275221
vision_sj@163.com <vision_sj@163.com>
2018-08-27 18:11:46 +0800

    修改框架页面

--- config/index.js
+++ config/index.js
@@ -8,57 +8,57 @@ const NODE_ENV = {
 		// CDN 服务器
 		CDN_ADDRESS: '/_nuxt/',
 		// API 服务器
-		SERVER_ADDRESS: 'http://api.jumoreys.com.pre', 
+		SERVER_ADDRESS: 'http://api.XXX.test', 
 		// 认证中心
-		USERCENTER_ADDRESS: 'https://passport.jumore.test', 
+		USERCENTER_ADDRESS: 'https://passport.XXX.test', 
 		// 中台地址
-		CENTER_ADDRESS: 'https://ecenter.jumore.test',
+		CENTER_ADDRESS: 'https://ecenter.XXX.test',
 		// 图片服务器
-		IMG_ADDRESS: 'https://img.jumore.com.pre',
+		IMG_ADDRESS: 'https://img.XXX.pre',
 		// 图片上传服务器
-		UPLOAD_ADDRESS: 'http://image5.jm.com'
+		UPLOAD_ADDRESS: 'http://image5.XXX.com'
 	},
 	test: {
 		// CDN 服务器
 		CDN_ADDRESS: '/_nuxt/',
 		// API 服务器
-		SERVER_ADDRESS: 'http://api.jumoreys.test',
+		SERVER_ADDRESS: 'http://api.XXX.test',
 		// 认证中心
-		USERCENTER_ADDRESS: 'https://passport.jumore.test',
+		USERCENTER_ADDRESS: 'https://passport.XXX.test',
 		// 中台地址
-		CENTER_ADDRESS: 'https://ecenter.jumore.test',
+		CENTER_ADDRESS: 'https://ecenter.XXX.test',
 		// 上传地址
-		UPLOAD_ADDRESS: 'http://image5.jm.com',
+		UPLOAD_ADDRESS: 'http://image5.XXX.com',
 		// 图片服务器
-		IMG_ADDRESS: 'https://image.jumore.test'
+		IMG_ADDRESS: 'https://image.XXX.test'
 	},
 	pre: {
 		// CDN 服务器
-		CDN_ADDRESS: 'https://www.jumoresp.com.pre/_gull/',
+		CDN_ADDRESS: 'https://www.XXX.pre/_gull/',
 		// API 服务器
-		SERVER_ADDRESS: 'http://api.jumoreys.com.pre',
+		SERVER_ADDRESS: 'http://api.XXX.pre',
 		// 认证中心
-		USERCENTER_ADDRESS: 'https://prepassport.jumore.com/cas',
+		USERCENTER_ADDRESS: 'https://prepassport.XXX.com/cas',
 		// 中台地址
-		CENTER_ADDRESS: 'https://ecenter.jumore.com.pre',
+		CENTER_ADDRESS: 'https://ecenter.XXX.pre',
 		// 上传地址
-		UPLOAD_ADDRESS: 'https://uploadapi.jumore.com.pre',
+		UPLOAD_ADDRESS: 'https://uploadapi.XXX.pre',
 		// 图片服务器
-		IMG_ADDRESS: 'https://img.jumore.com.pre'
+		IMG_ADDRESS: 'https://img.XXX.pre'
 	},
 	prod: {
 		// CDN 服务器
-		CDN_ADDRESS: 'https://www.jumoresp.com/_gull/',
+		CDN_ADDRESS: 'https://www.XXX.com/_gull/',
 		// API 服务器
-		SERVER_ADDRESS: 'http://api.jumoreys.com',
+		SERVER_ADDRESS: 'http://api.XXX.com',
 		// 认证中心
-		USERCENTER_ADDRESS: 'https://passport.jumore.com/cas',
+		USERCENTER_ADDRESS: 'https://passport.XXX.com/cas',
 		// 中台地址
-		CENTER_ADDRESS: 'https://ecenter.jumore.com',
+		CENTER_ADDRESS: 'https://ecenter.XXX.com',
 		// 上传地址
-		UPLOAD_ADDRESS: 'https://uploadapi.jumore.com',
+		UPLOAD_ADDRESS: 'https://uploadapi.XXX.com',
 		// 图片服务器
-		IMG_ADDRESS: 'https://img.jumore.com'
+		IMG_ADDRESS: 'https://img.XXX.com'
 	}
 }
 
